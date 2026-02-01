c465ede0a3e0234a63682a1af317a47c7032b9ef
Arafat-Dev <md489288@gmail.com>
2026-02-01 12:36:20 +0600

    caching:: add default cache options

--- src/services/medicine-service.ts
+++ src/services/medicine-service.ts
@@ -1,11 +1,42 @@
 import { env } from "@/env";
-import { TResult } from "@/types";
+import { FilterOptions, TResult } from "@/types";
 import { IMedicine } from "@/types/medicine-type";
 
+interface ICacheOptions {
+  cache?: RequestCache;
+  revalidate?: number;
+}
+
 export const medicineService = {
-  getMedicines: async (): Promise<TResult<IMedicine[]>> => {
+  getMedicines: async (
+    filter?: FilterOptions,
+    cacheOptions?: ICacheOptions,
+  ): Promise<TResult<IMedicine[]>> => {
     const API_URL = `${env.BACKEND_URL}/api/v1/medicines/`;
-    const response = await fetch(API_URL);
+    const url = new URL(API_URL);
+
+    if (filter) {
+      Object.entries(filter).forEach(([key, value]) => {
+        if (value !== undefined && value !== null && value !== "") {
+          url.searchParams.append(key, value.toString());
+        }
+      });
+    }
+
+    const config: RequestInit = {
+      cache: "default",
+      next: { revalidate: 1 * 60 * 60 * 24 },
+    };
+
+    if (cacheOptions?.cache) {
+      config.cache = cacheOptions.cache;
+    }
+
+    if (cacheOptions?.revalidate) {
+      config.next = { revalidate: cacheOptions.revalidate };
+    }
+
+    const response = await fetch(url.toString(), config);
     const result = await response.json();
 
     return {
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,28 @@
 export { ROLE } from "./enum";
 export type { TResult } from "./return-type";
 export type { Session, SessionWithUser, User } from "./session-type";
+
+export const MedicineStatus = {
+  AVAILABLE: "AVAILABLE",
+  OUT_OF_STOCK: "OUT_OF_STOCK",
+  DISCONTINUED: "DISCONTINUED",
+} as const;
+export interface FilterOptions {
+  searchTerm: string | undefined;
+  status: (typeof MedicineStatus)[keyof typeof MedicineStatus] | undefined;
+  categoryId: string | undefined;
+  id: string | undefined;
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+}
+
+export interface PaginationOptions {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+}
