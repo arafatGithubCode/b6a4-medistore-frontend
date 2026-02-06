0464c15b2d7680a509ea91d26d95a91fb85209ca
Arafat-Dev <md489288@gmail.com>
2026-02-06 21:29:39 +0600

    add demo accounts credentials

--- src/app/(common-layout)/signin/page.tsx
+++ src/app/(common-layout)/signin/page.tsx
@@ -19,6 +19,26 @@ const SigninPage = () => {
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
+      <div className="p-6 md:p-10">
+        <h3>Demo Accounts</h3>
+        <div className="space-y-4">
+          <div>
+            <p className="font-semibold">Admin Account:</p>
+            <p>Email: admin@medistore.com</p>
+            <p>Password: Admin@1234</p>
+          </div>
+          <div>
+            <p className="font-semibold">Customer Account:</p>
+            <p>Email: customer@medistore.com</p>
+            <p>Password: password1234</p>
+          </div>
+          <div>
+            <p className="font-semibold">Seller Account:</p>
+            <p>Email: seller@medistore.com</p>
+            <p>Password: password1234</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
